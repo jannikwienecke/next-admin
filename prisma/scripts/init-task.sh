16694b2946aab17a7853fef401cddaf64860dfe6
jannikwienecke <wienecke.jannik@gmail.com>
2023-08-25 13:04:51 +0200

    fix init script

--- prisma/scripts/init-task.sh
+++ prisma/scripts/init-task.sh
@@ -1,1 +1,5 @@
-cat ./prisma/task.sql | docker exec -i 944be97d5194  psql -U postgres -d db
+# ~ docker container ls  | grep 'postgres' | awk '{print $1}'
+
+CONTAINER_NAME=$(docker container ls  | grep 'postgres' | awk '{print $1}')
+
+ cat ./prisma/task.sql | docker exec -i $CONTAINER_NAME  psql -U postgres -d db
